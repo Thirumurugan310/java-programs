94cfe3716f8790848e02c52d7c0abf5ad30e45d2
Thirumurugan310 <thiruthiru33120@gmail.com>
2025-12-17 11:59:14 +0530

    Add files via upload

---
+++ java-looping---number-based/To multiply two numbers without using  operator/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    double n = sc.nextDouble();
+	    int m = sc.nextInt();
+	    int sum = 0;
+	    for(int i =0;i<m;i++){
+	        sum +=n;
+	    }
+	    System.out.println(sum);
+	}
+}
